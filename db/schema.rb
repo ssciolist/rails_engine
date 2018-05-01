58335e362cbb2cb2e5f2abe0eb1d3e46ac240f0d
Megan Arellano <ssciolist@gmail.com>
2018-05-01 14:30:33 -0600

    writes rake import all task, confirms functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180501185912) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +55,7 @@ ActiveRecord::Schema.define(version: 20180501185912) do
   end
 
   create_table "merchants", force: :cascade do |t|
-    t.citext "name"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
