a678b946721350771f1742eaa67ddc05af7de3a0
Megan Arellano <ssciolist@gmail.com>
2018-05-03 14:17:04 -0600

    adds serializer to pass spec harness

--- app/models/item.rb
+++ app/models/item.rb
@@ -31,5 +31,11 @@ class Item < ApplicationRecord
   end
 
   def self.best_day(item_id)
+    unscoped
+    .select('invoices.updated_at')
+    .joins(invoices: :transactions)
+    .where(transactions: {result: 'success'}, items: {id: item_id})
+    .order('invoice_items.quantity DESC')
+    .limit(1)
   end
 end
