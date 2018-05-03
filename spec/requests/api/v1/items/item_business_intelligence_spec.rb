b59c2a58dede13d0ba1481e3283f3a000736c619
Megan Arellano <ssciolist@gmail.com>
2018-05-03 10:53:13 -0600

    writes failing test

---
+++ spec/requests/api/v1/items/item_business_intelligence_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Item API business intelligence' do
+  it 'returns the top x item instances ranked by total number sold' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    item1 = merchant.items.create!(attributes_for(:item))
+    item2 = merchant.items.create!(attributes_for(:item))
+    item3 = merchant.items.create!(attributes_for(:item))
+    invoice = merchant.invoices.create!(attributes_for(:invoice_with_transactions, merchant: merchant, customer_id: customer.id))
+    invoice.invoice_items.create!(attributes_for(:invoice_item, item_id: item1.id, quantity: 5))
+    invoice.invoice_items.create!(attributes_for(:invoice_item, item_id: item2.id, quantity: 3))
+    invoice.invoice_items.create!(attributes_for(:invoice_item, item_id: item3.id, quantity: 15))
+
+    get "/api/v1/items/most_items?quantity=2"
+
+    top_items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(top_items.length).to eq(2)
+    expect(top_items.first.description).to eq(item3.description)
+    expect(top_items.last.description).to eq(item1.description)
+  end
+end
