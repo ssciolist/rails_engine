ff98f2b16383496a110c0986c4ccaf68ff8b0b17
jrambold <jrambold@gmail.com>
2018-05-03 09:11:07 -0600

    readded scoping to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,13 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  default_scope {order(:id)}
+
   def self.most_revenue(group_size)
     joins(:invoice_items, invoices: :transactions)
     .where(transactions: {result: "success"})
     .group(:id)
-    .order("sum(invoice_items.quantity * invoice_items.unit_price)")
+    .order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
     .limit(group_size)
   end
 end
